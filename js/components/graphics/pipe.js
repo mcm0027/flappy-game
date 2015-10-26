8754ca526e9d276e188704271a2cfb060f342acc
Matt Morris <mcm0027@yahoo.com>
2015-10-26 16:51:46 -0500

    add new graphics for pipes and birds

--- js/components/graphics/pipe.js
+++ js/components/graphics/pipe.js
@@ -2,8 +2,20 @@ var PipeGraphicsComponent = function(entity) {
   this.entity = entity;
 };
 
-PipeGraphicsComponent.prototype.draw = function() {
- console.log("Drawing a pipe"); 
+PipeGraphicsComponent.prototype.draw = function(context) {
+  context.beginPath();
+  context.arc(30, 100, 10, 0, 2 * Math.PI);
+  context.fill();
+  
+  context.beginPath();
+  context.arc(300, 150, 30, 0, 2 * Math.PI);
+  context.fill();
+  
+  context.shadowColor = "black";
+  context.shadowBlur = 10;
+
+  context.fillStyle = "white";
+  context.fillRect(10, 10, 100, 100);
 };
 
 exports.PipeGraphicsComponent = PipeGraphicsComponent;
